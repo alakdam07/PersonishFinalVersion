b132ccb89cd681f1ba3559915a680b2f0c434bbb
AlakDam <47625626+alakdam07@users.noreply.github.com>
2020-01-03 13:31:24 +0200

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const sequelize = require("./node_modules/sequelize");
 
-var con = new sequelize("image", "postgres", "Joymaaloknath123", {
+var con = new sequelize("image", "postgres", "password", {
   host: "localhost",
   dialect: "postgres",
 
